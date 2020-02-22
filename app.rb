5b69c030885784e760106c277720e79841f6bc88
github username <github email address>
2020-02-22 14:30:05 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
+    configure do
+        enable :sessions
+        set :session_secret, "secret"
+    end
+
+    get '/' do
+        @session = session
+        
+        erb :index
+    end
+
+    post '/checkouts' do
+         @item = params["item"]
+          session[:item] = @item
+          binding.pry
+    
+        erb :'checkout'
+    end
+
 end
